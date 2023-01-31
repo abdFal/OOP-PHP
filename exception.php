e8422596d066395f5e8c6aa554f9db817913d986
unknown <naufaldec07@gmail.com>
2023-01-31 11:31:13 +0700

    Update OOP

---
+++ exception.php
@@ -0,0 +1,20 @@
+<?php
+
+try {
+  // code that might throw an exception
+  $file = fopen("non_existent_file.txt", "r");
+  if (!$file) {
+    throw new Exception("File not found");
+  }
+  // other code here
+} catch (Exception $e) {
+  // exception handling code
+  echo "An exception was caught: " . $e->getMessage();
+} finally {
+  // code to be executed regardless of exception
+  if ($file) {
+    fclose($file);
+  }
+}
+
+?>
